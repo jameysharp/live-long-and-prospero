5f420b6dce34c2390a04ea5fdbb1998a1e4d6c7d
Jamey Sharp <jamey@minilop.net>
2025-04-12 01:09:43 -0700

    Basic IR and parser for fidget models

---
+++ src/ir/mod.rs
@@ -0,0 +1,166 @@
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::fmt;
+use std::hash::Hash;
+use std::ops::BitOr;
+
+mod reader;
+pub use reader::read;
+
+#[derive(Clone, Copy, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Const(u32);
+
+impl Const {
+    pub fn new(v: f32) -> Const {
+        assert!(v.is_finite());
+        Const(v.to_bits())
+    }
+
+    pub fn value(self) -> f32 {
+        f32::from_bits(self.0)
+    }
+}
+
+impl fmt::Debug for Const {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value())
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum UnOp {
+    Neg,
+    Square,
+    Sqrt,
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Min,
+    Max,
+}
+
+pub type InstIdx = u16;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum Inst {
+    Const { value: Const },
+    Var { idx: u8 },
+    Load,
+    UnOp { op: UnOp, arg: InstIdx },
+    BinOp { op: BinOp, args: [InstIdx; 2] },
+}
+
+#[derive(Clone, Copy, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct VarSet(u8);
+
+impl fmt::Debug for VarSet {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut v = self.0;
+        if v == 0 {
+            f.write_str("const")
+        } else {
+            while v != 0 {
+                let idx = v.trailing_zeros();
+                write!(f, "{}", char::from(b"xyzabcde"[idx as usize]))?;
+                v &= !(1 << idx);
+            }
+            Ok(())
+        }
+    }
+}
+
+impl BitOr for VarSet {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self {
+        VarSet(self.0 | rhs.0)
+    }
+}
+
+#[derive(Default)]
+pub struct Insts {
+    pool: Vec<Inst>,
+    vars: Vec<VarSet>,
+    gvn: HashMap<Inst, InstIdx>,
+}
+
+impl Insts {
+    pub fn push(&mut self, mut inst: Inst) -> InstIdx {
+        match &mut inst {
+            Inst::UnOp {
+                op: UnOp::Square,
+                arg,
+            } => {
+                // Squaring -x is the same as squaring x.
+                if let Inst::UnOp {
+                    op: UnOp::Neg,
+                    arg: pos,
+                } = self.pool[usize::from(*arg)]
+                {
+                    *arg = pos;
+                }
+            }
+
+            Inst::BinOp { op, args } => {
+                match op {
+                    // Sort arguments to commutative binary operators so GVN is more effective.
+                    BinOp::Add | BinOp::Mul | BinOp::Min | BinOp::Max => args.sort_unstable(),
+
+                    // Subtraction is not commutative, but if we previously subtracted the
+                    // arguments in the opposite order then we can just negate that previous
+                    // result.
+                    BinOp::Sub => {
+                        let [a, b] = *args;
+                        let reversed = Inst::BinOp {
+                            op: BinOp::Sub,
+                            args: [b, a],
+                        };
+                        if let Some(&idx) = self.gvn.get(&reversed) {
+                            inst = Inst::UnOp {
+                                op: UnOp::Neg,
+                                arg: idx,
+                            };
+                        }
+                    }
+                }
+            }
+            _ => (),
+        }
+
+        let vars = |idx: InstIdx| self.vars[usize::from(idx)];
+
+        match self.gvn.entry(inst) {
+            // If we've already added the same instruction, reuse it.
+            Entry::Occupied(e) => *e.get(),
+
+            Entry::Vacant(e) => {
+                self.vars.push(match *e.key() {
+                    Inst::Const { .. } => VarSet(0),
+                    Inst::Var { idx } => VarSet(1 << idx),
+                    Inst::UnOp { arg, .. } => vars(arg),
+                    Inst::BinOp { args: [a, b], .. } => vars(a) | vars(b),
+                    Inst::Load => panic!("use Insts::load to create load instructions"),
+                });
+
+                let idx = self.pool.len().try_into().unwrap();
+                self.pool.push(e.key().clone());
+                *e.insert(idx)
+            }
+        }
+    }
+
+    pub fn load(&mut self, vars: VarSet) -> InstIdx {
+        let idx = self.pool.len();
+        self.pool.push(Inst::Load);
+        self.vars.push(vars);
+        idx.try_into().unwrap()
+    }
+
+    pub fn vars(&self, idx: InstIdx) -> VarSet {
+        self.vars[usize::from(idx)]
+    }
+}
---
+++ src/ir/reader.rs
@@ -0,0 +1,121 @@
+use std::collections::{HashMap, hash_map::Entry};
+use std::io;
+use std::num::ParseFloatError;
+use std::str::SplitAsciiWhitespace;
+use thiserror::Error;
+
+use super::{BinOp, Const, Inst, InstIdx, Insts, UnOp};
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("read failed")]
+    IO(#[from] io::Error),
+    #[error("invalid constant")]
+    InvalidConst(#[from] ParseFloatError),
+    #[error("missing token")]
+    MissingToken,
+    #[error("unexpected token {0:?}")]
+    ExtraToken(String),
+    #[error("argument uses undefined name {0:?}")]
+    UndefinedName(String),
+    #[error("instruction redefines existing name {0:?}")]
+    RedefinedName(String),
+    #[error("unknown instruction {0:?}")]
+    UnknownOp(String),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+pub fn read(f: impl io::BufRead) -> Result<Insts> {
+    let mut insts = Insts::default();
+    let mut names = HashMap::new();
+
+    for line in f.lines() {
+        let line = line?;
+        if line.starts_with('#') {
+            continue;
+        }
+
+        let mut tokens = Tokens {
+            names: &names,
+            tokens: line.split_ascii_whitespace(),
+        };
+
+        let out = tokens.next()?;
+        let inst = match tokens.next()? {
+            "const" => Inst::Const {
+                value: Const::new(tokens.next()?.parse()?),
+            },
+
+            "var-x" => Inst::Var { idx: 0 },
+            "var-y" => Inst::Var { idx: 1 },
+            "var-z" => Inst::Var { idx: 2 },
+
+            "neg" => tokens.unop(UnOp::Neg)?,
+            "square" => tokens.unop(UnOp::Square)?,
+            "sqrt" => tokens.unop(UnOp::Sqrt)?,
+
+            "add" => tokens.binop(BinOp::Add)?,
+            "sub" => tokens.binop(BinOp::Sub)?,
+            "mul" => tokens.binop(BinOp::Mul)?,
+            "min" => tokens.binop(BinOp::Min)?,
+            "max" => tokens.binop(BinOp::Max)?,
+
+            op => return Err(Error::UnknownOp(op.to_string())),
+        };
+
+        tokens.empty()?;
+
+        match names.entry(out.to_string()) {
+            Entry::Vacant(e) => {
+                e.insert(insts.push(inst));
+            }
+            Entry::Occupied(e) => {
+                return Err(Error::RedefinedName(e.remove_entry().0));
+            }
+        }
+    }
+
+    Ok(insts)
+}
+
+struct Tokens<'a> {
+    names: &'a HashMap<String, InstIdx>,
+    tokens: SplitAsciiWhitespace<'a>,
+}
+
+impl<'a> Tokens<'a> {
+    fn next(&mut self) -> Result<&'a str> {
+        self.tokens.next().ok_or_else(|| Error::MissingToken)
+    }
+
+    fn arg(&mut self) -> Result<InstIdx> {
+        let name = self.next()?;
+        self.names
+            .get(name)
+            .ok_or_else(|| Error::UndefinedName(name.to_string()))
+            .copied()
+    }
+
+    fn unop(&mut self, op: UnOp) -> Result<Inst> {
+        Ok(Inst::UnOp {
+            op,
+            arg: self.arg()?,
+        })
+    }
+
+    fn binop(&mut self, op: BinOp) -> Result<Inst> {
+        Ok(Inst::BinOp {
+            op,
+            args: [self.arg()?, self.arg()?],
+        })
+    }
+
+    fn empty(mut self) -> Result<()> {
+        if let Some(next) = self.tokens.next() {
+            Err(Error::ExtraToken(next.to_string()))
+        } else {
+            Ok(())
+        }
+    }
+}
