5bd32597ee14b98c2ec8dc5de6a70102e99fc103
Jamey Sharp <jamey@minilop.net>
2025-04-16 00:47:44 -0700

    Refactor reassociation so I can justify its correctness more easily

--- src/ir/reassociate.rs
+++ src/ir/reassociate.rs
@@ -10,275 +10,282 @@ pub fn reassociate(insts: &mut Insts) {
     // further, vars(a) == vars(b) implies one of these:
     //   a does not use the same operator
     //   a or b has multiple uses
-    let mut uses = vec![Saturating(0u8); insts.pool.len()];
-    if let Some(last) = uses.last_mut() {
+    let mut state = State {
+        vars: &mut insts.vars,
+        uses: vec![Saturating(0u8); insts.pool.len()],
+        stack: Vec::new(),
+    };
+
+    if let Some(last) = state.uses.last_mut() {
         last.0 = 1;
     }
     for (idx, inst) in insts.pool.iter().enumerate().rev() {
-        if uses[idx].0 > 0 {
+        if state.uses[idx].0 > 0 {
             for &arg in inst.args() {
-                uses[usize::from(arg)] += 1;
+                state.uses[usize::from(arg)] += 1;
             }
         }
     }
 
-    let mut stack = Vec::new();
-    for idx in 0..InstIdx::try_from(insts.len()).unwrap() {
-        stack.push(idx);
-        while let Some(root) = stack.pop() {
-            if let Inst::BinOp { op, args: [a, b] } = &mut insts.pool[usize::from(root)] {
-                if a == b {
-                    // if both args are the same, their vars are equal, and we'd
-                    // blow up in get_disjoint_mut below. can't reassociate this
-                    // case without duplicating instructions anyway.
-                    continue;
+    for idx in 0..InstIdx::try_from(insts.pool.len()).unwrap() {
+        if let Inst::BinOp { op, args } = &mut insts.pool[usize::from(idx)] {
+            state.visit_binop(idx, *op, args);
+            while let Some((op, indexes)) = state.stack.pop() {
+                fn subtree(indexes: [InstIdx; 3], insts: &mut [Inst]) -> [&mut Inst; 3] {
+                    insts.get_disjoint_mut(indexes.map(usize::from)).unwrap()
                 }
 
-                match insts.vars[usize::from(*a)].cmp(insts.vars[usize::from(*b)]) {
-                    Ordering::Equal => simplify(*op, [root, *a, *b], &mut stack, &uses, insts),
-                    Ordering::Greater if op.is_commutative() => swap(a, b),
-                    _ => {}
-                }
+                let [ir, ia, ib] = subtree(indexes, &mut insts.pool);
+
+                let new_root = state.extract_neg(indexes, op, ia, ib);
+                let mut subtree = if let Some((new_root, r, [a, b])) = new_root {
+                    *ir = new_root;
+                    debug_assert_ne!(indexes, [r, a, b]);
+                    subtree([r, a, b], &mut insts.pool)
+                } else {
+                    [ir, ia, ib]
+                };
+
+                state.rebalance_add_or_sub(&mut subtree);
+                state.rebalance_commutative(subtree);
             }
         }
     }
 }
 
-fn simplify(
-    op: BinOp,
-    indexes: [InstIdx; 3],
-    stack: &mut Vec<InstIdx>,
-    uses: &[Saturating<u8>],
-    insts: &mut Insts,
-) {
-    fn subtree(indexes: [InstIdx; 3], insts: &mut [Inst]) -> [&mut Inst; 3] {
-        insts.get_disjoint_mut(indexes.map(usize::from)).unwrap()
-    }
-
-    let [r, a, b] = indexes;
-    let [ir, ia, ib] = subtree(indexes, &mut insts.pool);
-    let reuse_a = uses[usize::from(a)].0 < 2;
-    let reuse_b = uses[usize::from(b)].0 < 2;
-
-    let replace = |op, args| Some((Inst::BinOp { op, args }, r, args));
-
-    let mut negate = |op, arg, iarg: &mut Inst, args| {
-        *iarg = Inst::BinOp { op, args };
-        insts.vars[usize::from(arg)] = insts.vars[usize::from(r)];
-        Some((Inst::UnOp { op: UnOp::Neg, arg }, arg, args))
-    };
-
-    let indexes = match (op, ia.is_unop(UnOp::Neg), ib.is_unop(UnOp::Neg)) {
-        // (-x) + y = y - x
-        (BinOp::Add, Some(na), None) => replace(BinOp::Sub, [b, na]),
-        // x + (-y) = x - y
-        (BinOp::Add, None, Some(nb)) => replace(BinOp::Sub, [a, nb]),
-        // (-x) + (-y) = -(x + y)
-        (BinOp::Add, Some(na), Some(nb)) if reuse_a => negate(BinOp::Add, a, ia, [na, nb]),
-        (BinOp::Add, Some(na), Some(nb)) if reuse_b => negate(BinOp::Add, b, ib, [na, nb]),
-
-        // (-x) - y = -(x + y)
-        (BinOp::Sub, Some(na), None) if reuse_a => negate(BinOp::Add, a, ia, [na, b]),
-        // x - (-y) = x + y
-        (BinOp::Sub, None, Some(nb)) => replace(BinOp::Add, [a, nb]),
-        // (-x) - (-y) = y - x
-        (BinOp::Sub, Some(na), Some(nb)) => replace(BinOp::Sub, [nb, na]),
-
-        // (-x) * y = -(x * y)
-        (BinOp::Mul, Some(na), None) if reuse_a => negate(BinOp::Mul, a, ia, [na, b]),
-        // x * (-y) = -(x * y)
-        (BinOp::Mul, None, Some(nb)) if reuse_b => negate(BinOp::Mul, b, ib, [a, nb]),
-        // (-x) * (-y) = x * y
-        (BinOp::Mul, Some(na), Some(nb)) => replace(BinOp::Mul, [na, nb]),
-
-        // min(-x, -y) = -max(x, y)
-        (BinOp::Min, Some(na), Some(nb)) if reuse_a => negate(BinOp::Max, a, ia, [na, nb]),
-        (BinOp::Min, Some(na), Some(nb)) if reuse_b => negate(BinOp::Max, b, ib, [na, nb]),
-        // max(-x, -y) = -min(x, y)
-        (BinOp::Max, Some(na), Some(nb)) if reuse_a => negate(BinOp::Min, a, ia, [na, nb]),
-        (BinOp::Max, Some(na), Some(nb)) if reuse_b => negate(BinOp::Min, b, ib, [na, nb]),
-
-        _ => None,
-    };
-
-    let mut subtree = if let Some((new_root, r, [a, b])) = indexes {
-        *ir = new_root;
-        subtree([r, a, b], &mut insts.pool)
-    } else {
-        [ir, ia, ib]
-    };
-    rebalance_add_or_sub(&mut subtree, stack, uses);
-    rebalance_commutative(&mut insts.vars, uses, stack, subtree);
+struct State<'a> {
+    vars: &'a mut [VarSet],
+    uses: Vec<Saturating<u8>>,
+    stack: Vec<(BinOp, [InstIdx; 3])>,
 }
 
-fn rebalance_add_or_sub(
-    [inst, inst1, inst2]: &mut [&mut Inst; 3],
-    stack: &mut Vec<InstIdx>,
-    uses: &[Saturating<u8>],
-) -> Option<()> {
-    fn add_or_sub(inst: &mut Inst) -> Option<(bool, &mut [InstIdx; 2])> {
-        match inst {
-            Inst::BinOp { op, args } => match op {
-                BinOp::Add => Some((true, args)),
-                BinOp::Sub => Some((false, args)),
-                _ => None,
-            },
+impl State<'_> {
+    fn vars(&self, root: InstIdx) -> VarSet {
+        self.vars[usize::from(root)]
+    }
+
+    fn can_reuse(&self, root: InstIdx) -> bool {
+        self.uses[usize::from(root)].0 < 2
+    }
+
+    fn visit_binop(&mut self, root: InstIdx, op: BinOp, [a, b]: &mut [InstIdx; 2]) {
+        // if both args are the same, their vars are equal, and we'd blow up
+        // in get_disjoint_mut. can't reassociate that case without duplicating
+        // instructions anyway.
+        if a != b {
+            match self.vars(*a).cmp(self.vars(*b)) {
+                Ordering::Greater if op.is_commutative() => swap(a, b),
+                Ordering::Equal => self.stack.push((op, [root, *a, *b])),
+                _ => {}
+            }
+        }
+    }
+
+    fn extract_neg(
+        &mut self,
+        [r, a, b]: [InstIdx; 3],
+        op: BinOp,
+        ia: &mut Inst,
+        ib: &mut Inst,
+    ) -> Option<(Inst, InstIdx, [InstIdx; 2])> {
+        let replace = |op, args| Some((Inst::BinOp { op, args }, r, args));
+
+        let mut negate = |op, arg, iarg: &mut Inst, args| {
+            self.can_reuse(arg).then(|| {
+                *iarg = Inst::BinOp { op, args };
+                self.vars[usize::from(arg)] = self.vars(r);
+                (Inst::UnOp { op: UnOp::Neg, arg }, arg, args)
+            })
+        };
+
+        match (op, ia.is_unop(UnOp::Neg), ib.is_unop(UnOp::Neg)) {
+            // (-x) + y = y - x
+            (BinOp::Add, Some(na), None) => replace(BinOp::Sub, [b, na]),
+            // x + (-y) = x - y
+            (BinOp::Add, None, Some(nb)) => replace(BinOp::Sub, [a, nb]),
+            // (-x) + (-y) = -(x + y)
+            (BinOp::Add, Some(na), Some(nb)) => {
+                negate(BinOp::Add, a, ia, [na, nb]).or_else(|| negate(BinOp::Add, b, ib, [na, nb]))
+            }
+
+            // (-x) - y = -(x + y)
+            (BinOp::Sub, Some(na), None) => negate(BinOp::Add, a, ia, [na, b]),
+            // x - (-y) = x + y
+            (BinOp::Sub, None, Some(nb)) => replace(BinOp::Add, [a, nb]),
+            // (-x) - (-y) = y - x
+            (BinOp::Sub, Some(na), Some(nb)) => replace(BinOp::Sub, [nb, na]),
+
+            // (-x) * y = -(x * y)
+            (BinOp::Mul, Some(na), None) => negate(BinOp::Mul, a, ia, [na, b]),
+            // x * (-y) = -(x * y)
+            (BinOp::Mul, None, Some(nb)) => negate(BinOp::Mul, b, ib, [a, nb]),
+            // (-x) * (-y) = x * y
+            (BinOp::Mul, Some(na), Some(nb)) => replace(BinOp::Mul, [na, nb]),
+
+            // min(-x, -y) = -max(x, y)
+            (BinOp::Min, Some(na), Some(nb)) => {
+                negate(BinOp::Max, a, ia, [na, nb]).or_else(|| negate(BinOp::Max, b, ib, [na, nb]))
+            }
+            // max(-x, -y) = -min(x, y)
+            (BinOp::Max, Some(na), Some(nb)) => {
+                negate(BinOp::Min, a, ia, [na, nb]).or_else(|| negate(BinOp::Min, b, ib, [na, nb]))
+            }
+
             _ => None,
         }
     }
 
-    let (sign1, [w, x]) = add_or_sub(inst1)?;
-    let (sign2, [y, z]) = add_or_sub(inst2)?;
-
-    // if both subterms are addition, use the general case
-    if sign1 && sign2 {
-        return None;
-    }
-
-    let (sign, [a, b]) = add_or_sub(inst)?;
-
-    // if either subterm is a subtraction and has other uses, we can't rewrite
-    // this subtree
-    if !sign1 && uses[usize::from(*a)].0 > 1 {
-        return None;
-    }
-    if !sign2 && uses[usize::from(*b)].0 > 1 {
-        return None;
-    }
-
-    let (args, args1, args2) = if sign2 {
-        debug_assert!(!sign1);
-        if sign {
-            // (w - x) + (y + z) = (w + (y + z)) - x
-            ([*a, *x], Some([*w, *b]), None)
-        } else {
-            // (w - x) - (y + z) = w - (x + (y + z))
-            ([*w, *a], Some([*x, *b]), None)
+    fn rebalance_add_or_sub(&mut self, [inst, inst1, inst2]: &mut [&mut Inst; 3]) -> Option<()> {
+        fn add_or_sub(inst: &mut Inst) -> Option<(bool, &mut [InstIdx; 2])> {
+            match inst {
+                Inst::BinOp { op, args } => match op {
+                    BinOp::Add => Some((true, args)),
+                    BinOp::Sub => Some((false, args)),
+                    _ => None,
+                },
+                _ => None,
+            }
         }
-    } else {
-        if !sign {
-            // a - (y - z) = a + (z - y)
-            swap(y, z);
+
+        let (sign1, [w, x]) = add_or_sub(inst1)?;
+        let (sign2, [y, z]) = add_or_sub(inst2)?;
+
+        // if both subterms are addition, use the general case
+        if sign1 && sign2 {
+            return None;
         }
-        if sign1 {
-            // (w + x) + (y - z) = ((w + x) + y) - z
-            ([*b, *z], None, Some([*a, *y]))
-        } else {
-            // (w - x) + (y - z) = (w + y) - (x + z)
-            ([*a, *b], Some([*w, *y]), Some([*x, *z]))
+
+        let (sign, [a, b]) = add_or_sub(inst)?;
+
+        // if either subterm is a subtraction and has other uses, we can't rewrite
+        // this subtree
+        if !sign1 && !self.can_reuse(*a) {
+            return None;
         }
-    };
+        if !sign2 && !self.can_reuse(*b) {
+            return None;
+        }
 
-    // now rewrite subterms, adding them to the stack to revisit if changed
-    let op = BinOp::Add;
-    debug_assert_eq!(args1.is_none(), sign1);
-    if let Some(args) = args1 {
-        stack.push(*a);
-        **inst1 = Inst::BinOp { op, args };
-    }
-    debug_assert_eq!(args2.is_none(), sign2);
-    if let Some(args) = args2 {
-        stack.push(*b);
-        **inst2 = Inst::BinOp { op, args };
-    }
-
-    // and rewrite the root
-    let op = BinOp::Sub;
-    **inst = Inst::BinOp { op, args };
-
-    Some(())
-}
-
-fn rebalance_commutative(
-    vars: &mut [VarSet],
-    uses: &[Saturating<u8>],
-    stack: &mut Vec<InstIdx>,
-    [inst, inst1, inst2]: [&mut Inst; 3],
-) {
-    // rematch the (possibly rewritten) instruction under the new borrow
-    let Inst::BinOp { op, args } = inst else {
-        unreachable!()
-    };
-
-    if !op.is_commutative() {
-        return;
-    }
-    if let Some(args1) = inst1.is_binop_mut(*op) {
-        if let Some(args2) = inst2.is_binop_mut(*op) {
-            // don't modify inst1 or inst2 if they have other
-            // uses besides in inst, and at this point we can't
-            // reassociate without modifying both
-            let [idx1, idx2] = args.map(usize::from);
-            if uses[idx1].0 < 2 && uses[idx2].0 < 2 {
-                rebalance(stack, vars, args, args1, args2);
+        let (args, args1, args2) = if sign2 {
+            debug_assert!(!sign1);
+            if sign {
+                // (w - x) + (y + z) = (w + (y + z)) - x
+                ([*a, *x], Some([*w, *b]), None)
+            } else {
+                // (w - x) - (y + z) = w - (x + (y + z))
+                ([*w, *a], Some([*x, *b]), None)
             }
         } else {
-            // inst1 matched but inst2 did not, and they have the
-            // same vars, so swap them to establish the invariant
-            let [a, b] = args;
-            swap(a, b);
+            if !sign {
+                // a - (y - z) = a + (z - y)
+                swap(y, z);
+            }
+            if sign1 {
+                // (w + x) + (y - z) = ((w + x) + y) - z
+                ([*b, *z], None, Some([*a, *y]))
+            } else {
+                // (w - x) + (y - z) = (w + y) - (x + z)
+                ([*a, *b], Some([*w, *y]), Some([*x, *z]))
+            }
+        };
+
+        // now rewrite subterms, adding them to the stack to revisit if changed
+        let op = BinOp::Add;
+        debug_assert_eq!(args1.is_none(), sign1);
+        if let Some(mut args) = args1 {
+            self.visit_binop(*a, op, &mut args);
+            **inst1 = Inst::BinOp { op, args };
+        }
+        debug_assert_eq!(args2.is_none(), sign2);
+        if let Some(mut args) = args2 {
+            self.visit_binop(*b, op, &mut args);
+            **inst2 = Inst::BinOp { op, args };
+        }
+
+        // and rewrite the root
+        let op = BinOp::Sub;
+        **inst = Inst::BinOp { op, args };
+
+        Some(())
+    }
+
+    fn rebalance_commutative(&mut self, [inst, inst1, inst2]: [&mut Inst; 3]) {
+        // rematch the (possibly rewritten) instruction under the new borrow
+        let Inst::BinOp { op, args } = inst else {
+            unreachable!()
+        };
+
+        if !op.is_commutative() {
+            return;
+        }
+
+        let [a, b] = args;
+        if let Some(args1) = inst1.is_binop_mut(*op) {
+            if let Some(args2) = inst2.is_binop_mut(*op) {
+                // don't modify inst1 or inst2 if they have other
+                // uses besides in inst, and at this point we can't
+                // reassociate without modifying both
+                if self.can_reuse(*a) && self.can_reuse(*b) {
+                    self.rebalance(*op, args, args1, args2);
+                }
+            } else {
+                // inst1 matched but inst2 did not, and they have the
+                // same vars, so swap them to establish the invariant
+                swap(a, b);
+            }
         }
     }
-}
 
-fn rebalance(
-    stack: &mut Vec<InstIdx>,
-    vars: &mut [VarSet],
-    [a, b]: &mut [InstIdx; 2],
-    [a1, b1]: &mut [InstIdx; 2],
-    [a2, b2]: &mut [InstIdx; 2],
-) {
-    // we've matched a happy little balanced tree:
-    //   inst(a=inst1(a1, b1), b=inst2(a2, b2))
+    fn rebalance(
+        &mut self,
+        op: BinOp,
+        args: &mut [InstIdx; 2],
+        args1: &mut [InstIdx; 2],
+        args2: &mut [InstIdx; 2],
+    ) {
+        // we've matched a happy little balanced tree:
+        //   inst(inst1(a1, b1), inst2(a2, b2))
+        // all three instructions use the same operator and, inductively, inst1
+        // and inst2 already satisfy our invariant.
 
-    let mut varsa1 = vars[usize::from(*a1)];
-    let mut varsa2 = vars[usize::from(*a2)];
-    let varsb1 = vars[usize::from(*b1)];
-    let varsb2 = vars[usize::from(*b2)];
+        let [inst1, inst2] = *args;
+        let [a1, b1] = *args1;
+        let [a2, b2] = *args2;
+        let leaves = [b1, b2, a1, a2].map(|idx| (self.vars(idx), idx));
 
-    if varsa1 == varsa2 && (varsa1 < varsb1 || varsa2 < varsb2) {
-        // we know:
-        //   vars(a1) == vars(a2) -- by this check; call this "little"
-        // so if we group a1 with a2 the result will have
-        // the same vars as little. but what happens if we
-        // group b1 with b2?
-        //   little < vars(b1) or little < vars(b2) -- by this check
-        //   vars(b1) <= vars(b1) | vars(b2) -- by x <= x | y
-        //   vars(b2) <= vars(b1) | vars(b2) -- by x <= x | y
-        //   little   <  vars(b1) | vars(b2) -- by x < y <= z implies x < z
-        // this satisifies our invariant.
-        stack.extend([*a, *b]);
-        vars[usize::from(*a)] = varsa1;
-        vars[usize::from(*b)] = varsb1 | varsb2;
-        swap(b1, a2);
-    } else {
-        // we want to unbalance this tree into a linked list:
-        //   inst(a=a1, b=inst1(a1=a2, b1=inst2(a2=b1, b2)))
+        let mut insts = [(InstIdx::MAX, args), (inst1, args1), (inst2, args2)]
+            .into_iter()
+            .rev();
+        let mut merge = |old: &mut InstIdx, new| {
+            if *old == InstIdx::MAX {
+                *old = new;
+            } else {
+                let (inst, args) = insts.next().unwrap();
+                let new_args = [new, *old];
+                *old = inst;
+                if new_args != *args {
+                    *args = new_args;
+                    if inst != InstIdx::MAX {
+                        let [a, b] = new_args;
+                        self.vars[usize::from(inst)] = self.vars(a) | self.vars(b);
+                        self.visit_binop(inst, op, args);
+                    }
+                }
+            }
+        };
 
-        let vars2 = varsb1 | varsb2;
-        // inst1 satisfies the invariant, namely
-        //   vars(a2) <= vars(b1) | vars(b2)
-        debug_assert!(varsa2 <= vars2);
-        // because vars(a2) <= vars(b2) (or vars(b1) if we
-        // swapped a1/a2 above) and
-        //   x <= y implies x <= y | z
-        // so we don't need to look at inst1 again here,
-        // only inst2
-        stack.push(*b);
-        vars[usize::from(*a)] = vars2 | varsa2;
-        vars[usize::from(*b)] = vars2;
-
-        // swap a1/a2 if necessary so they're in ascending order
-        if varsa1 > varsa2 {
-            swap(a1, a2);
-            swap(&mut varsa1, &mut varsa2);
+        let mut groups = [InstIdx::MAX; VarSet::ALL.idx() + 1];
+        for (vars, idx) in leaves {
+            merge(&mut groups[vars.idx()], idx);
         }
 
-        let tmp = *b;
-        *b = *a;
-        *a = *a1;
-        *a1 = *a2;
-        *a2 = *b1;
-        *b1 = tmp;
+        let mut last = InstIdx::MAX;
+        for group in groups.into_iter().rev() {
+            if group != InstIdx::MAX {
+                merge(&mut last, group);
+            }
+        }
+        debug_assert_eq!(last, InstIdx::MAX);
+        debug_assert_eq!(insts.next(), None);
     }
 }
