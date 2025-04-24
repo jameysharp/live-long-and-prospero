759faa600c58e9f4e1aaf147d71703bd6ecf729e
Jamey Sharp <jamey@minilop.net>
2025-04-23 23:47:37 -0700

    Don't reorder automatically in reassociate example
    It was necessary previously but isn't now, so separate the two passes to
    make it easier to evaluate them independently.

--- examples/reassociate.rs
+++ examples/reassociate.rs
@@ -2,8 +2,7 @@ use geometry_compiler::ir;
 
 fn main() -> ir::io::Result<()> {
     let insts = ir::io::read(std::io::stdin().lock(), ir::Insts::default())?;
-    let mut insts = ir::reassociate::reassociate(&insts.pool, ir::Insts::default());
-    ir::reorder::reorder(&mut insts);
+    let insts = ir::reassociate::reassociate(&insts.pool, ir::Insts::default());
     ir::io::write(std::io::stdout().lock(), insts.pool.iter().cloned())?;
     Ok(())
 }
